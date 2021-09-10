5fffa2bd00dc1bef3e0a58bec78bcf5e2342dc07
Dominick Simone <domsimone123@gmail.com>
2021-09-09 20:49:13 -0400

    boiler plate

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,11 @@
 const router = require("express").Router()
+
+const homepageRoutes = require("./homepage-routes")
+const loginRoutes = require("./login-routes")
+const leaderboardRoutes = require("./leaderboard-routes")
+
+router.use("/", homepageRoutes);
+router.use("/login", loginRoutes);
+router.use("/leaderboard", leaderboardRoutes);
+
+module.exports = router;
