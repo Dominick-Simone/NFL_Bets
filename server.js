9c2fca816bef7726864a9f8585622e0e1706be8d
Dominick Simone <domsimone123@gmail.com>
2021-09-18 09:46:38 -0400

    adding morgan

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const morgan = require('morgan')
 
 const sequelize = require('./config/connection');
 
@@ -33,6 +34,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('tiny'))
 
 app.use(routes);
 
